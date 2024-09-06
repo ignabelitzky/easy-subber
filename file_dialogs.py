41c5c6994dac72d4883be0645dffaa5a48ebfef3
Ignacio Belitzky <ignabelitzky@gmail.com>
2024-09-06 16:06:56 -0300

    initialdir variable added to the filefialogs

--- file_dialogs.py
+++ file_dialogs.py
@@ -5,6 +5,7 @@ def select_video_file():
     """Open a file dialog to select a video file."""
     video_file_dialog = filedialog.askopenfilename(
         title="Select a video file",
+        initialdir="~/",
         filetypes=[("Video files", "*.mp4 *.avi *.mkv")]
     )
     return video_file_dialog
@@ -13,6 +14,7 @@ def save_subtitle_file():
     """Open a file dialog to select the save location."""
     save_path = filedialog.asksaveasfilename(
         title="Save Subtitle As",
+        initialdir="~/",
         defaultextension=".srt",
         filetypes=[("Subtitle files", "*.srt")]
     )
